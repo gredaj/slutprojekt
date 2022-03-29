95c1fc1852332290d3ce3de263789c5f9bbbf39f
gredaj <94680414+gredaj@users.noreply.github.com>
2022-03-29 11:11:05 +0200

    Add files via upload

---
+++ hemsida.js
@@ -0,0 +1,16 @@
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
